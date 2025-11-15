42a08561f3253aea630378dd22f3b8712231b98e
Iouri “Georg” Haller <georg07haller@gmail.com>
2025-11-15 19:07:15 +0000

    File changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { Heart, Crown } from 'lucide-react';
 
 export default function Footer() {
